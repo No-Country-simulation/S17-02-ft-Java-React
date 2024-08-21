7d47ece4dfeb37cac27af43aba16476c8071072a
catriel <catriellescogar@gmail.com>
2024-08-21 14:39:32 -0300

    Estructura inicial del proyecto

---
+++ api/src/main/java/com/nocountry/telemedicina/TelemedicinaApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.telemedicina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedicinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemedicinaApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/nocountry/telemedicina/TelemedicinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.telemedicina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedicinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
