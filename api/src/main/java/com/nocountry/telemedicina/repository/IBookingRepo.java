99bcdd3e8978e2fad4c7f7f313d5185c609a27a5
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-22 14:55:24 -0500

    repositori generic y repository Bookin y role

---
+++ api/src/main/java/com/nocountry/telemedicina/repository/IBookingRepo.java
@@ -0,0 +1,8 @@
+package com.nocountry.telemedicina.repository;
+
+import com.nocountry.telemedicina.models.Booking;
+
+import java.util.UUID;
+
+public interface IBookingRepo extends IGenericRepo<Booking, UUID>{
+}
--- api/src/main/java/com/nocountry/telemedicina/repository/IRoleRepo.java
+++ api/src/main/java/com/nocountry/telemedicina/repository/IRoleRepo.java
@@ -4,6 +4,5 @@ import com.nocountry.telemedicina.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface IRoleRepo extends JpaRepository<Role,Long> {
+public interface IRoleRepo extends IGenericRepo<Role, Long> {
 }
