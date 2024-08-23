4aff633fde00a66f206656ad1adfcb3b024d3ac2
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-23 12:11:34 -0500

    specialty model

--- api/src/main/java/com/nocountry/telemedicina/models/Specialty.java
+++ api/src/main/java/com/nocountry/telemedicina/models/Specialty.java
@@ -1,15 +1,12 @@
 package com.nocountry.telemedicina.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Setter
 @Getter
@@ -29,4 +26,7 @@ public class Specialty extends Auditable{
     
     @Column(name = "specialty_description",nullable = false,length = 80)
     private String specialtyDescription;
+
+    @OneToMany
+    private List<Specialist>specialists;
 }
