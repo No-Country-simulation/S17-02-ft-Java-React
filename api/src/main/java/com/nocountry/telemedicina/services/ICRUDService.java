b1982a37ec1c6ef1387b598eee295d5b6692cc0d
Rogelio Olarte <galaxino12@gmail.com>
2024-08-23 12:10:18 -0500

    added updatedById and findAll functions

--- api/src/main/java/com/nocountry/telemedicina/services/ICRUDService.java
+++ api/src/main/java/com/nocountry/telemedicina/services/ICRUDService.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 public interface ICRUDService<T,ID> {
     T save(T t);
     T findById(ID id);
-    Page<T> findAll(int page, int size);
+    T updateById(ID id, T t);
     void deleteById(ID id);
+    Page<T> findAll(int page, int size);
 }
