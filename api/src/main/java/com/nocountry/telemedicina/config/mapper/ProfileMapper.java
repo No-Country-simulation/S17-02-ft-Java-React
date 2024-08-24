a6f7f2c39b21f576e04d47ae6ca5140ad0505b22
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-24 15:22:18 -0500

    Profile Response dto

--- api/src/main/java/com/nocountry/telemedicina/config/mapper/ProfileMapper.java
+++ api/src/main/java/com/nocountry/telemedicina/config/mapper/ProfileMapper.java
@@ -1,4 +1,23 @@
 package com.nocountry.telemedicina.config.mapper;
 
-public class ProfileMapper {
+import com.nocountry.telemedicina.dto.response.ProfileResponseDTO;
+import com.nocountry.telemedicina.models.Profile;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface ProfileMapper {
+
+    ProfileMapper INSTANCE = Mappers.getMapper(ProfileMapper.class);
+
+    @Mapping(source = "user.username",target = "email")
+    @Mapping(source = "user.userId",target = "userId")
+    @Mapping(source = "district.districtName",target = "districtName")
+    ProfileResponseDTO toProfileDTO(Profile profile);
+
+    @Mapping(source = "email",target = "user.username")
+    @Mapping(source = "userId",target = "user.userId")
+    @Mapping(source = "districtName",target = "district.districtName")
+    Profile toProfile(ProfileResponseDTO profileDTO);
 }
