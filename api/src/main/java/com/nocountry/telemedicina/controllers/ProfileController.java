92e84f4285aa8ff2a8b1b62511d7b2611a4e381e
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-24 14:52:34 -0500

    profile controller

---
+++ api/src/main/java/com/nocountry/telemedicina/controllers/ProfileController.java
@@ -0,0 +1,30 @@
+package com.nocountry.telemedicina.controllers;
+
+import com.nocountry.telemedicina.dto.response.ProfileResponseDTO;
+import com.nocountry.telemedicina.exception.NotAuthorizedException;
+import com.nocountry.telemedicina.models.Profile;
+import com.nocountry.telemedicina.services.IProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/profiles")
+public class ProfileController {
+
+    @Autowired
+    private IProfileService service;
+
+    public ResponseEntity<ProfileResponseDTO>findById(@PathVariable("id") UUID id){
+        Profile obj = service.findById(id);
+        if(obj == null){
+            throw new NotAuthorizedException("ID NOT FOUND: " + id);
+        }else {
+            //return new ResponseEntity<>(obj)
+        }
+    }
+}
