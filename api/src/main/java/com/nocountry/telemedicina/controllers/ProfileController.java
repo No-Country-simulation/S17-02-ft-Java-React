d7f0a30facb7feeaff849acb7fe30d37cc182636
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-24 15:25:14 -0500

    Profile controller findById usando mapper

--- api/src/main/java/com/nocountry/telemedicina/controllers/ProfileController.java
+++ api/src/main/java/com/nocountry/telemedicina/controllers/ProfileController.java
@@ -1,10 +1,12 @@
 package com.nocountry.telemedicina.controllers;
 
+import com.nocountry.telemedicina.config.mapper.ProfileMapper;
 import com.nocountry.telemedicina.dto.response.ProfileResponseDTO;
 import com.nocountry.telemedicina.exception.NotAuthorizedException;
 import com.nocountry.telemedicina.models.Profile;
 import com.nocountry.telemedicina.services.IProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +21,15 @@ public class ProfileController {
     @Autowired
     private IProfileService service;
 
+    @Autowired
+    private ProfileMapper mapper;
+
     public ResponseEntity<ProfileResponseDTO>findById(@PathVariable("id") UUID id){
         Profile obj = service.findById(id);
         if(obj == null){
             throw new NotAuthorizedException("ID NOT FOUND: " + id);
         }else {
-            //return new ResponseEntity<>(obj)
+            return new ResponseEntity<>(mapper.toProfileDTO(obj), HttpStatus.OK);
         }
     }
 }
