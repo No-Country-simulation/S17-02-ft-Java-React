f3ae50baecc2467b48f9af32fca5657cea3667e6
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-24 19:05:21 -0500

    Clinic Response DTO

--- api/src/main/java/com/nocountry/telemedicina/dto/response/ClinicResponseDTO.java
+++ api/src/main/java/com/nocountry/telemedicina/dto/response/ClinicResponseDTO.java
@@ -3,5 +3,10 @@ package com.nocountry.telemedicina.dto.response;
 import java.util.List;
 import java.util.UUID;
 
-public record ClinicResponseDTO(UUID clinicId,String clinicName,UserResponseDTO user,List<SubsidiaryResponseDTO> subsidiaries,List<SpecialistResponseDTO> specialists) {
+public record ClinicResponseDTO(
+        UUID clinicId,
+        String clinicName,
+        UUID userId,
+        String username,
+        List<SubsidiaryResponseDTO> subsidiaries) {
 }
