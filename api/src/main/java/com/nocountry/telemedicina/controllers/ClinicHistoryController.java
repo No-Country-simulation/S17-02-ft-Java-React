8495773c52ff4b016b328fa0c793de85d8c19118
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-25 14:20:27 -0500

    Clinical History Controller: create,delete,listAll,findById,Update

--- api/src/main/java/com/nocountry/telemedicina/controllers/ClinicHistoryController.java
+++
@@ -1,9 +0,0 @@
-package com.nocountry.telemedicina.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/clinic-histories")
-public class ClinicHistoryController {
-}
