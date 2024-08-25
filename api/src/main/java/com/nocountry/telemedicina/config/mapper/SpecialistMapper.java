4389760e0cda5580103d69cd77e93cdea4f19232
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-25 12:49:00 -0500

    Specialist Mapper

---
+++ api/src/main/java/com/nocountry/telemedicina/config/mapper/SpecialistMapper.java
@@ -0,0 +1,24 @@
+package com.nocountry.telemedicina.config.mapper;
+
+import com.nocountry.telemedicina.dto.request.SpecialistRequestDTO;
+import com.nocountry.telemedicina.dto.response.SpecialistResponseDTO;
+import com.nocountry.telemedicina.models.Specialist;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface SpecialistMapper {
+
+    SpecialistMapper INSTANCE = Mappers.getMapper(SpecialistMapper.class);
+
+    @Mapping(source = "clinic.clinicId",target = "clinicId")
+    @Mapping(source = "clinic.clinicName",target = "clinicName")
+    @Mapping(source = "specialty.name",target = "specialtyName")
+    @Mapping(source = "specialty.id",target = "specialtyId")
+    SpecialistResponseDTO toSpecialistDto(Specialist specialist);
+
+    @Mapping(source = "clinic",target = "clinic")
+    @Mapping(source = "specialty",target = "specialty")
+    Specialist toSpecialist(SpecialistRequestDTO dto);
+}
