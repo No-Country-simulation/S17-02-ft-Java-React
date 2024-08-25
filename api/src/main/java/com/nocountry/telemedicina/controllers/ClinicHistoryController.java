c8ce5ebaf7d8ae38ef8bcf4a056ee9c7d0108a73
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-24 20:58:57 -0500

    Creacion del controlador de las historias clinicas

---
+++ api/src/main/java/com/nocountry/telemedicina/controllers/ClinicHistoryController.java
@@ -0,0 +1,9 @@
+package com.nocountry.telemedicina.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clinic-histories")
+public class ClinicHistoryController {
+}
