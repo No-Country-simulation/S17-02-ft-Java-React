5fb8d23c0a24b22e7b04e68b2fdc8a3e72505e19
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-25 14:57:52 -0500

    Update name ISpecialtyService

--- api/src/main/java/com/nocountry/telemedicina/services/ISpecialtyService.java
+++ api/src/main/java/com/nocountry/telemedicina/services/ISpecialtyService.java
@@ -2,5 +2,5 @@ package com.nocountry.telemedicina.services;
 
 import com.nocountry.telemedicina.models.Specialty;
 
-public interface ISpecialityService extends ICRUDService<Specialty, Long> {
+public interface ISpecialtyService extends ICRUDService<Specialty, Long> {
 }
