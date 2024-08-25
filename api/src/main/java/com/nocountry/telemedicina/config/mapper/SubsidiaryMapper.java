99fb4c3f011ad9e49d5bf946396ba1ebc6d0c071
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-25 13:57:41 -0500

    Subsidiary Mapper

---
+++ api/src/main/java/com/nocountry/telemedicina/config/mapper/SubsidiaryMapper.java
@@ -0,0 +1,23 @@
+package com.nocountry.telemedicina.config.mapper;
+
+import com.nocountry.telemedicina.dto.request.SubsidiaryRequestDTO;
+import com.nocountry.telemedicina.dto.response.SubsidiaryResponseDTO;
+import com.nocountry.telemedicina.models.Subsidiary;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface SubsidiaryMapper {
+
+    SubsidiaryMapper INSTANCE = Mappers.getMapper(SubsidiaryMapper.class);
+
+    @Mapping(source = "district.districtName",target = "districtName")
+    @Mapping(source = "clinic.clinicId",target = "clinicId")
+    @Mapping(source = "clinic.clinicName",target = "clinicName")
+    SubsidiaryResponseDTO toSubsidiaryDto(Subsidiary subsidiary);
+
+    @Mapping(source = "district",target = "district")
+    @Mapping(source = "clinic",target = "clinic")
+    Subsidiary toSubsidiary(SubsidiaryRequestDTO dto);
+}
