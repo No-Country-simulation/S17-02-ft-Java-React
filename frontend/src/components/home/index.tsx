eafc1249550abaa2a49a7cb23c97860155cb64a2
PHRCOOK <https://pabloelleproso@gmail.com>
2024-08-26 10:44:43 -0300

    actualizacion componente home

--- frontend/src/components/home/index.tsx
+++ frontend/src/components/home/index.tsx
@@ -7,45 +7,40 @@ import RegisterClinic from "../registerClinic/index.tsx";
 import RegisterEspecialist from "../registerEspecialist/index.tsx";
 
 const Home: React.FC = () => {
-  const [showRegisterUser, setShowRegisterUser] = useState<boolean>(false);
-  const [showRegisterClinic, setShowRegisterClinic] = useState<boolean>(false);
-  const [showRegisterEspecialist, setShowRegisterEspecialist] =
-    useState<boolean>(false);
+  // State to determine which form is currently shown
+  const [activeForm, setActiveForm] = useState<string | null>(null);
 
-  const toggleRegisterUser = () => {
-    setShowRegisterUser((prev) => !prev);
-  };
-
-  const toggleRegisterClinic = () => {
-    setShowRegisterClinic((prev) => !prev);
-  };
-
-  const toggleRegisterEspecialist = () => {
-    setShowRegisterEspecialist((prev) => !prev);
+  // Function to open a specific form and close others
+  const toggleForm = (form: string) => {
+    setActiveForm((prevForm) => (prevForm === form ? null : form));
   };
 
   return (
     <div>
       <Header />
       <NavBar />
-      <button onClick={toggleRegisterUser}>
-        {showRegisterUser
+      <button onClick={() => toggleForm("user")}>
+        {activeForm === "user"
           ? "Cerrar Registro de Usuario"
           : "Registro de Usuario"}
       </button>
-      {showRegisterUser && <RegisterUser />}
-      <button onClick={toggleRegisterClinic}>
-        {showRegisterClinic
+      {activeForm === "user" && <RegisterUser />}
+
+      <button onClick={() => toggleForm("clinic")}>
+        {activeForm === "clinic"
           ? "Cerrar Registro de Clínica"
           : "Registro de Clínica"}
       </button>
-      {showRegisterClinic && <RegisterClinic />}
-      <button onClick={toggleRegisterEspecialist}>
-        {showRegisterEspecialist
+      {activeForm === "clinic" && <RegisterClinic />}
+
+      <button onClick={() => toggleForm("especialist")}>
+        {activeForm === "especialist"
           ? "Cerrar Registro de Especialista"
           : "Registro de Especialista"}
       </button>
-      {showRegisterEspecialist && <RegisterEspecialist />} <Footer />
+      {activeForm === "especialist" && <RegisterEspecialist />}
+
+      <Footer />
     </div>
   );
 };
