fea56be7c537d11e78d57625a11848b3e392195e
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-30 00:16:21 -0500

    Query de findAllByUserId

--- api/src/main/java/com/nocountry/telemedicina/repository/IBookingRepo.java
+++ api/src/main/java/com/nocountry/telemedicina/repository/IBookingRepo.java
@@ -1,7 +1,15 @@
 package com.nocountry.telemedicina.repository;
 
 import com.nocountry.telemedicina.models.Booking;
+import com.nocountry.telemedicina.repository.projection.IBookingProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.UUID;
 
 public interface IBookingRepo extends IGenericRepo<Booking, UUID>{
+
+    @Query(value = "SELECT * FROM bookings WHERE user_id=:id and active=true", nativeQuery = true)
+    Page<IBookingProjection> findAllByUserId(UUID id, Pageable pageable);
 }
