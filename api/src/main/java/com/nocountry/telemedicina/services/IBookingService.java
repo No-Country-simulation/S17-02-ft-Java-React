813c7693fe8ea7f8c80935be7e685eacc01a5c08
Anderson Cusma <andersoncusma20@gmail.com>
2024-08-30 00:16:48 -0500

    Servicio de findAllByUserId

--- api/src/main/java/com/nocountry/telemedicina/services/IBookingService.java
+++ api/src/main/java/com/nocountry/telemedicina/services/IBookingService.java
@@ -1,8 +1,12 @@
 package com.nocountry.telemedicina.services;
 
 import com.nocountry.telemedicina.models.Booking;
+import com.nocountry.telemedicina.repository.projection.IBookingProjection;
+import org.springframework.data.domain.Page;
 
 import java.util.UUID;
 
 public interface IBookingService extends ICRUDService<Booking, UUID> {
+    Page<IBookingProjection> findAllByUserId(UUID id, int page, int size, String sortField, String sortOrder);
+
 }
