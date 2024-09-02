1e4856bbf61b98ec923f2443ed68f5936524e558
catriel <catriellescobar@gmail.com>
2024-09-02 14:38:55 -0300

    mercadopago checkout pro

--- api/src/main/java/com/nocountry/telemedicina/services/impl/MercadoPagoServiceImpl.java
+++ api/src/main/java/com/nocountry/telemedicina/services/impl/MercadoPagoServiceImpl.java
@@ -1,11 +1,104 @@
 package com.nocountry.telemedicina.services.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.mercadopago.MercadoPagoConfig;
+import com.mercadopago.client.common.IdentificationRequest;
+import com.mercadopago.client.payment.PaymentClient;
+import com.mercadopago.client.payment.PaymentCreateRequest;
+import com.mercadopago.client.payment.PaymentPayerRequest;
+import com.mercadopago.client.preference.PreferenceBackUrlsRequest;
+import com.mercadopago.client.preference.PreferenceClient;
+import com.mercadopago.client.preference.PreferenceItemRequest;
+import com.mercadopago.client.preference.PreferenceRequest;
+import com.mercadopago.exceptions.MPApiException;
+import com.mercadopago.exceptions.MPException;
+import com.mercadopago.resources.payment.Payment;
+import com.mercadopago.resources.preference.Preference;
+import com.nocountry.telemedicina.dto.request.CardPaymentDTO;
+import com.nocountry.telemedicina.dto.request.ItemPreferenceDTO;
 
 @Service
 public class MercadoPagoServiceImpl {
 
-    @Value("${mercadopago.access_token}")
-    private String mercadoPagoToken;
+    @Value("${mercadopago.token.pro}")
+    private String mercadoPagoTokenPro;
+    @Value("${mercadopago.token.brick}")
+    private String mercadoPagoTokenBrick;
+
+    public Payment cardPayment(CardPaymentDTO request) throws MPException {
+        try {
+            MercadoPagoConfig.setAccessToken(mercadoPagoTokenBrick);
+            PaymentCreateRequest paymentCreateRequest = PaymentCreateRequest.builder()
+                    .transactionAmount(request.getTransactionAmount())
+                    .token(request.getToken())
+                    .installments(request.getInstallments())
+                    .paymentMethodId(request.getPaymentMethodId())
+                    .payer(
+                            PaymentPayerRequest.builder()
+                                    .email(request.getPayer().getEmail())
+                                    .identification(
+                                            IdentificationRequest.builder()
+                                                    .type(request.getPayer().getIdentification().getType())
+                                                    .number(request.getPayer().getIdentification().getNumber())
+                                                    .build())
+                                    .build())
+                    .build();
+
+            PaymentClient client = new PaymentClient();
+
+            Payment pago = client.create(paymentCreateRequest);
+            return pago;
+        } catch (MPException | MPApiException e) {
+            throw new MPException(e.getMessage());
+        }
+    }
+
+    public String generatePreferenceId(List<ItemPreferenceDTO> productos, String username) {
+        try {
+            MercadoPagoConfig.setAccessToken(mercadoPagoTokenPro);
+
+            // TODO AGREGAR EL ID DEL SCHEDULE PARA LUEGO DE LA COMPRA CREAR BOOKING
+
+            List<PreferenceItemRequest> items = productos.stream().map(producto -> PreferenceItemRequest.builder()
+                    .id(producto.getId().toString())
+                    .title(producto.getTitle())
+                    .description(producto.getDescription())
+                    .categoryId(producto.getCategoryId())
+                    .quantity(1)
+                    .currencyId("ARS") // "ARS, PE"
+                    .unitPrice(producto.getUnitPrice())
+                    .build()).collect(Collectors.toList());
+
+            PreferenceBackUrlsRequest backUrls = PreferenceBackUrlsRequest.builder()
+                    .success("http://localhost:5173/")
+                    .pending("http://localhost:5173/")
+                    .failure("http://localhost:5173/")
+                    .build();
+
+            // TODO BUSCAR AL USUARIO POR EMAIL Y AGREGARLO PAYER
+
+            PreferenceRequest preferenceRequest = PreferenceRequest.builder()
+                    .items(items)
+                    .additionalInfo("HeyDoc")
+                    .notificationUrl(
+                            "https://0d36-179-38-157-244.ngrok-free.app/api/mercadoPago/notify")
+                    .backUrls(backUrls)
+                    .autoReturn("approved")
+                    .statementDescriptor("HeyDoc")
+                    .expires(true)
+                    .marketplace("HeyDoc")
+                    .build();
+
+            PreferenceClient client = new PreferenceClient();
+            Preference preference = client.create(preferenceRequest);
+            return preference.getId();
+        } catch (MPException | MPApiException error) {
+            return error.toString();
+        }
+    }
 }
