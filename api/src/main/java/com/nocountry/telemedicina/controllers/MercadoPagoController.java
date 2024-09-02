4a58df49f8d92ed2f38dca847d3bebb9b78bf51b
catriel <catriellescobar@gmail.com>
2024-09-02 17:41:31 -0300

    Mercado pago, TODO: implementar mp en el front

--- api/src/main/java/com/nocountry/telemedicina/controllers/MercadoPagoController.java
+++ api/src/main/java/com/nocountry/telemedicina/controllers/MercadoPagoController.java
@@ -2,23 +2,16 @@ package com.nocountry.telemedicina.controllers;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
 import com.nocountry.telemedicina.dto.request.ItemPreferenceDTO;
+import com.nocountry.telemedicina.dto.request.PaymentInfoDTO;
 import com.nocountry.telemedicina.dto.request.WebHookDTO;
 import com.nocountry.telemedicina.services.impl.MercadoPagoServiceImpl;
-
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -45,26 +38,10 @@ public class MercadoPagoController {
         String type = webHookDTO.getType();
         String url = String.format("https://api.mercadopago.com/v1/payments/%s", paymentId);
         if (type.equals("payment")) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setBearerAuth(bearerTokenPro);
-            HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<Map<String, Object>> response = restTemplate.exchange(url,
-                    HttpMethod.GET, requestEntity,
-                    (Class<Map<String, Object>>) (Class<?>) Map.class);
-
-            log.info(response.toString());
-            return ResponseEntity.ok(response);
+            PaymentInfoDTO paymentInfoDTO = mercadoPagoServiceImpl.getInfoMp(url);
+            log.info(paymentInfoDTO.toString());
+            return ResponseEntity.ok(paymentInfoDTO);
         }
         return ResponseEntity.ok("que miras maquina");
     }
-
-    // @PostMapping("/webhook")
-    // public ResponseEntity<?> webhook(
-    // @RequestBody Map<String, Object> request) {
-
-    // log.info(request.toString());
-
-    // return ResponseEntity.ok("hola");
-    // }
 }
--- api/src/main/java/com/nocountry/telemedicina/services/impl/MercadoPagoServiceImpl.java
+++ api/src/main/java/com/nocountry/telemedicina/services/impl/MercadoPagoServiceImpl.java
@@ -2,11 +2,17 @@ package com.nocountry.telemedicina.services.impl;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+import java.util.Map;
 import com.nocountry.telemedicina.exception.BadRequestException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mercadopago.MercadoPagoConfig;
 import com.mercadopago.client.common.IdentificationRequest;
 import com.mercadopago.client.payment.PaymentClient;
@@ -22,80 +28,108 @@ import com.mercadopago.resources.payment.Payment;
 import com.mercadopago.resources.preference.Preference;
 import com.nocountry.telemedicina.dto.request.CardPaymentDTO;
 import com.nocountry.telemedicina.dto.request.ItemPreferenceDTO;
+import com.nocountry.telemedicina.dto.request.PaymentInfoDTO;
 
 @Service
+@Slf4j
 public class MercadoPagoServiceImpl {
 
-    @Value("${mercadopago.token.pro}")
-    private String mercadoPagoTokenPro;
-    @Value("${mercadopago.token.brick}")
-    private String mercadoPagoTokenBrick;
+        @Value("${mercadopago.token.pro}")
+        private String mercadoPagoTokenPro;
+        @Value("${mercadopago.token.brick}")
+        private String mercadoPagoTokenBrick;
 
-    public Payment cardPayment(CardPaymentDTO request) throws MPException {
-        try {
-            MercadoPagoConfig.setAccessToken(mercadoPagoTokenBrick);
-            PaymentCreateRequest paymentCreateRequest = PaymentCreateRequest.builder()
-                    .transactionAmount(request.getTransactionAmount())
-                    .token(request.getToken())
-                    .installments(request.getInstallments())
-                    .paymentMethodId(request.getPaymentMethodId())
-                    .payer(
-                            PaymentPayerRequest.builder()
-                                    .email(request.getPayer().getEmail())
-                                    .identification(
-                                            IdentificationRequest.builder()
-                                                    .type(request.getPayer().getIdentification().getType())
-                                                    .number(request.getPayer().getIdentification().getNumber())
-                                                    .build())
-                                    .build())
-                    .build();
+        public Payment cardPayment(CardPaymentDTO request) throws MPException {
+                try {
+                        MercadoPagoConfig.setAccessToken(mercadoPagoTokenBrick);
+                        PaymentCreateRequest paymentCreateRequest = PaymentCreateRequest.builder()
+                                        .transactionAmount(request.getTransactionAmount())
+                                        .token(request.getToken())
+                                        .installments(request.getInstallments())
+                                        .paymentMethodId(request.getPaymentMethodId())
+                                        .payer(
+                                                        PaymentPayerRequest.builder()
+                                                                        .email(request.getPayer().getEmail())
+                                                                        .identification(
+                                                                                        IdentificationRequest.builder()
+                                                                                                        .type(request.getPayer()
+                                                                                                                        .getIdentification()
+                                                                                                                        .getType())
+                                                                                                        .number(request.getPayer()
+                                                                                                                        .getIdentification()
+                                                                                                                        .getNumber())
+                                                                                                        .build())
+                                                                        .build())
+                                        .build();
 
-            PaymentClient client = new PaymentClient();
+                        PaymentClient client = new PaymentClient();
 
-            Payment pago = client.create(paymentCreateRequest);
-            return pago;
-        } catch (MPException | MPApiException e) {
-            throw new MPException(e.getMessage());
+                        Payment pago = client.create(paymentCreateRequest);
+                        return pago;
+                } catch (MPException | MPApiException e) {
+                        throw new MPException(e.getMessage());
+                }
         }
-    }
 
-    public String generatePreferenceId(List<ItemPreferenceDTO> productos) {
-        try {
-            MercadoPagoConfig.setAccessToken(mercadoPagoTokenPro);
+        public String generatePreferenceId(List<ItemPreferenceDTO> productos) {
+                try {
+                        MercadoPagoConfig.setAccessToken(mercadoPagoTokenPro);
 
-            List<PreferenceItemRequest> items = productos.stream().map(producto -> PreferenceItemRequest.builder()
-                    .id(producto.getId().toString())
-                    .title(producto.getTitle())
-                    .description(producto.getDescription())
-                    .categoryId(producto.getCategoryId())
-                    .quantity(1)
-                    .currencyId("ARS") // "ARS, PE"
-                    .unitPrice(producto.getUnitPrice())
-                    .build()).collect(Collectors.toList());
+                        List<PreferenceItemRequest> items = productos.stream()
+                                        .map(producto -> PreferenceItemRequest.builder()
+                                                        .id(producto.getId().toString())
+                                                        .title(producto.getTitle())
+                                                        .description(producto.getDescription())
+                                                        .categoryId(producto.getCategoryId())
+                                                        .quantity(1)
+                                                        .currencyId("ARS") // "ARS, PE"
+                                                        .unitPrice(producto.getUnitPrice())
+                                                        .build())
+                                        .collect(Collectors.toList());
 
-            PreferenceBackUrlsRequest backUrls = PreferenceBackUrlsRequest.builder()
-                    .success("http://localhost:5173/")
-                    .pending("http://localhost:5173/")
-                    .failure("http://localhost:5173/")
-                    .build();
+                        PreferenceBackUrlsRequest backUrls = PreferenceBackUrlsRequest.builder()
+                                        .success("http://localhost:5173/")
+                                        .pending("http://localhost:5173/")
+                                        .failure("http://localhost:5173/")
+                                        .build();
 
-            PreferenceRequest preferenceRequest = PreferenceRequest.builder()
-                    .items(items)
-                    .additionalInfo("HeyDoc")
-                    .notificationUrl(
-                            "https://0d36-179-38-157-244.ngrok-free.app/api/mercadoPago/notify")
-                    .backUrls(backUrls)
-                    .autoReturn("approved")
-                    .statementDescriptor("HeyDoc")
-                    .expires(true)
-                    .marketplace("HeyDoc")
-                    .build();
+                        PreferenceRequest preferenceRequest = PreferenceRequest.builder()
+                                        .items(items)
+                                        .additionalInfo("HeyDoc")
+                                        .notificationUrl(
+                                                        "https://0d36-179-38-157-244.ngrok-free.app/api/mercadoPago/notify")
+                                        .backUrls(backUrls)
+                                        .autoReturn("approved")
+                                        .statementDescriptor("HeyDoc")
+                                        .expires(true)
+                                        .marketplace("HeyDoc")
+                                        .build();
 
-            PreferenceClient client = new PreferenceClient();
-            Preference preference = client.create(preferenceRequest);
-            return preference.getId();
-        } catch (MPException | MPApiException error) {
-            throw new BadRequestException(error.getMessage());
+                        PreferenceClient client = new PreferenceClient();
+                        Preference preference = client.create(preferenceRequest);
+                        return preference.getId();
+                } catch (MPException | MPApiException error) {
+                        throw new BadRequestException(error.getMessage());
+                }
         }
-    }
+
+        public PaymentInfoDTO getInfoMp(String url) {
+                try {
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.setBearerAuth(mercadoPagoTokenPro);
+                        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+                        RestTemplate restTemplate = new RestTemplate();
+                        ResponseEntity<Map<String, Object>> response = restTemplate.exchange(url,
+                                        HttpMethod.GET, requestEntity,
+                                        (Class<Map<String, Object>>) (Class<?>) Map.class);
+
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        PaymentInfoDTO paymentResponse = objectMapper.convertValue(response.getBody(),
+                                        PaymentInfoDTO.class);
+                        log.info(paymentResponse.toString());
+                        return paymentResponse;
+                } catch (Exception e) {
+                        throw new BadRequestException(e.getMessage());
+                }
+        }
 }
