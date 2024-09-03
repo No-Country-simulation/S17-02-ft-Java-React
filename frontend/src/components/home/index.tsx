42958be13e7f9c83bba29d3dc752d382b0f43a0f
Blas <blas.pacha@gmail.com>
2024-09-03 18:39:02 -0300

    Correcion de importaciones home

--- frontend/src/components/home/index.tsx
+++ frontend/src/components/home/index.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
-
-
 import { RegisterUser } from "../registerUser";
 import { RegisterClinic } from "../registerClinic";
 import { Link } from "react-router-dom";
