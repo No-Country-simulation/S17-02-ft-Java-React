52ab051a92a1b9b54ba4a919be9e28479e527501
Anderson Cusma <andersoncusma20@gmail.com>
2024-09-05 14:36:10 -0500

    buscar Turno por userId

--- api/src/main/java/com/nocountry/telemedicina/repository/ISchedulesRepo.java
+++ api/src/main/java/com/nocountry/telemedicina/repository/ISchedulesRepo.java
@@ -1,8 +1,20 @@
 package com.nocountry.telemedicina.repository;
 
 import com.nocountry.telemedicina.models.Schedules;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.UUID;
 
 
 public interface ISchedulesRepo extends IGenericRepo<Schedules, Long> {
-
+    @Query(value = "SELECT sc.active, sc.schedules_day, sc.schedules_duration, sc.schedules_end, sc.schedules_repeat, sc.schedules_rest, sc.schedules_start, sc.created_at, sc.deleted_at, sc.schedules_id, sc.updated_at, sc.create_by, sc.delete_by, sc.specialist_id, sc.update_by\n" +
+            "\tFROM schedules sc\n" +
+            "\tINNER JOIN specialists s\n" +
+            "\tON s.specialist_id=sc.specialist_id\n" +
+            "\tINNER JOIN profiles p\n" +
+            "\tON p.profile_id=s.profile_id\n" +
+            "\tINNER JOIN users u\n" +
+            "\tON u.user_id=p.user_id\n" +
+            "\tWHERE u.user_id=:userId",nativeQuery = true)
+    Schedules findByUserId(UUID userId);
 }
