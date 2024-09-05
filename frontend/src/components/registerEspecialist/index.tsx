a60ce2ba29e12094e34edc847a99ca9749864445
PHRCOOK <https://pabloelleproso@gmail.com>
2024-09-05 01:25:40 -0300

    registro especialista

--- frontend/src/components/registerEspecialist/index.tsx
+++ frontend/src/components/registerEspecialist/index.tsx
@@ -1,8 +1,31 @@
+import React from "react";
 import { useForm } from "./userForm";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export const RegisterEspecialist = () => {
-  const { formData, handleChange, handleSubmit } = useForm();
+export const RegisterEspecialist: React.FC = () => {
+  const { formData, handleChange } = useForm();
+
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const requestBody = {
+      username: formData.username,
+      password: formData.password,
+      rolesId: ["9c765b7d-9eec-421b-85c6-6d53bcd002da"],
+    };
+
+    try {
+      const response = await axios.post("/api/auth/register", requestBody, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("Registro exitoso:", response.data);
+    } catch (error) {
+      console.error("Error al registrar el usuario:", error);
+    }
+  };
 
   return (
     <div>
@@ -10,7 +33,7 @@ export const RegisterEspecialist = () => {
         <Link to="/">Inicio</Link>
       </nav>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleFormSubmit}>
         <h2>Registro de Usuario</h2>
 
         <label>
