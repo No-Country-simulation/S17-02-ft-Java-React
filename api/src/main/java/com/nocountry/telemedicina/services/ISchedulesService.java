bdc0ada7d166dd66cfcdd7438ee292d1f376737b
Anderson Cusma <andersoncusma20@gmail.com>
2024-09-05 14:36:56 -0500

    Nuevo servicio para guardar turnos

--- api/src/main/java/com/nocountry/telemedicina/services/ISchedulesService.java
+++ api/src/main/java/com/nocountry/telemedicina/services/ISchedulesService.java
@@ -1,6 +1,9 @@
 package com.nocountry.telemedicina.services;
 
 import com.nocountry.telemedicina.models.Schedules;
+import com.nocountry.telemedicina.security.oauth2.user.CurrentUser;
+import com.nocountry.telemedicina.security.oauth2.user.UserPrincipal;
 
 public interface ISchedulesService extends ICRUDService<Schedules, Long> {
+    Schedules save(Schedules schedules, @CurrentUser UserPrincipal user);
 }
