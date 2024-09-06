bded52c1c427f07c6d57f4e074c18bd529f25faa
PHRCOOK <https://pabloelleproso@gmail.com>
2024-09-06 15:41:12 -0300

    estilado de navbar pacientes y especialistas

--- frontend/src/components/patientsNavbar/index.tsx
+++ frontend/src/components/patientsNavbar/index.tsx
@@ -1,27 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/context";
 
 const NavbarPacientes: React.FC = () => {
+  const { token, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    logout();
+    navigate("/");
+  };
+
   return (
-    <nav>
-      <div>
-        <h1>Pacientes</h1>
-        <ul>
-          <li>
-            <Link to="/">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/pacientes">Pacientes</Link>
-          </li>
-          <li>
-            <Link to="/reportes">Reportes</Link>
-          </li>
-          <li>
-            <Link to="/configuracion">Configuración</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <header className="navbar p-4">
+      <nav className="bg-body-tertiar">
+        <button className="badge">HeyDoc!</button>
+
+        <div className="d-flex gap-2">
+          <div className="d-flex flex-column flex-md-row justify-content-center gap-4">
+            <Link to="/">
+              <button className="btn-navbar-prof">Inicio</button>
+            </Link>
+            <Link to="/pacientes">
+              <button className="btn-navbar-prof">Pacientes</button>
+            </Link>
+            <Link to="/reportes">
+              <button className="btn-navbar-prof">Reportes</button>
+            </Link>
+            <Link to="/configuracion">
+              <button className="btn-navbar-prof">Configuración</button>
+            </Link>
+            {token && (
+              <button onClick={handleLogout} className="btn-navbar-prof">
+                Logout
+              </button>
+            )}
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
--- frontend/src/components/specialistsNavbar/index.tsx
+++ frontend/src/components/specialistsNavbar/index.tsx
@@ -1,27 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/context";
 
 const NavbarEspecialista: React.FC = () => {
+  const { token, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    logout();
+    navigate("/");
+  };
+
   return (
-    <nav>
-      <div>
-        <h1>Especialistas</h1>
-        <ul>
-          <li>
-            <Link to="/">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/especialistas">Especialistas</Link>
-          </li>
-          <li>
-            <Link to="/citas">Citas</Link>
-          </li>
-          <li>
-            <Link to="/historial">Historial</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <header className="navbar p-4">
+      <nav className="bg-body-tertiar">
+        <button className="badge">HeyDoc!</button>
+
+        <div className="d-flex gap-2">
+          <div className="d-flex flex-column flex-md-row justify-content-center gap-4">
+            <Link to="/">
+              <button className="btn-navbar-prof">Inicio</button>
+            </Link>
+            <Link to="/especialistas">
+              <button className="btn-navbar-prof">Especialistas</button>
+            </Link>
+            <Link to="/citas">
+              <button className="btn-navbar-prof">Citas</button>
+            </Link>
+            <Link to="/historial">
+              <button className="btn-navbar-prof">Historial</button>
+            </Link>
+            {token && (
+              <button onClick={handleLogout} className="btn-navbar-prof">
+                Logout
+              </button>
+            )}
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
