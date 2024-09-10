476060cb9da72b2609ef0fa53511ef2beecb051c
FerJuan <fbatres@gmail.com>
2024-09-10 07:21:30 -0300

    trabajando en about, adaptando responsive

--- frontend/src/components/home/About/About.tsx
+++ frontend/src/components/home/About/About.tsx
@@ -1,25 +1,19 @@
-import image from '../../../assets/images/About/img_about.png'
 
 const About = () => {
   return (
-    <section className="d-flex justify-content-center flex-wrap mt-5 gap-3 gap-md-4">
-    <div className="row about-container">
-      <div className="col-md-6 col-12 p-5">
-        <h2 className="title mt-3 fs-2">Sobre Nosotros</h2>
-        <span className="subtitle mt-3 fs-2"><strong>HeyDoc es una plataforma que te ayuda a mejorar tu salud</strong></span>
-        <p className="mt-3 fs-3">
-          Pasa consulta online por chat y videollamada, emite recetas e informes,
-          envía citas y solicita cobros a tus pacientes. Todo desde una misma
-          plataforma.
-        </p>
-      </div>
-      <div className="col-md-6 col-12 p-5">
-        <div className="d-flex justify-content-center">
-          <img src={image} alt="images" className="w-75" />
-        </div>
+    
+        <div className="about-container">
+          <div className="about">
+            <h3 className="">Atencion medica de calidad a un clic de distancia</h3>
+            <h5 className="mt-3 w-75">
+              accede a consultas con especialistas médicos, desde la comodidad de tu hogar.
+            </h5>
+            <p className='w-75'>sin esperas ni desplazamientos innecesarios. desde HeyDoc! te conectas con profesionales de la salud 24/7, recibiendo atención rápida, segura y personalizada, cuando más la necesitas.</p>
+          </div>
+      <div className="image">
+          
       </div>
     </div>
-  </section>
   
   )
 }
