cb19e8febe851455bc2d2a0e6729a1a5caa736b6
Ferjuan <fbatres@gmail.com>
2024-09-10 12:42:40 -0300

    finalizado about y responsive

--- frontend/src/components/home/About/About.tsx
+++ frontend/src/components/home/About/About.tsx
@@ -5,10 +5,10 @@ const About = () => {
         <div className="about-container">
           <div className="about">
             <h3 className="">Atencion medica de calidad a un clic de distancia</h3>
-            <h5 className="mt-3 w-75">
+            <h5 className="">
               accede a consultas con especialistas médicos, desde la comodidad de tu hogar.
             </h5>
-            <p className='w-75'>sin esperas ni desplazamientos innecesarios. desde HeyDoc! te conectas con profesionales de la salud 24/7, recibiendo atención rápida, segura y personalizada, cuando más la necesitas.</p>
+            <p className=''>sin esperas ni desplazamientos innecesarios. desde HeyDoc! te conectas con profesionales de la salud 24/7, recibiendo atención rápida, segura y personalizada, cuando más la necesitas.</p>
           </div>
       <div className="image">
           
