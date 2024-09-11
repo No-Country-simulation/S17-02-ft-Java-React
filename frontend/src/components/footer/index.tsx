b78cc7b46a9226b235e47c6e0bb83e2d4f713df2
Ferjuan <fbatres@gmail.com>
2024-09-11 12:14:06 -0300

    footer responsive, falta agregar a donde se dirigen los enlaces

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2024 Hey Doctor!. Todos los derechos reservados.</p>
+      <div> 
+      <a to ="/" className="badge">HeyDoc!
+        </a>  
+      </div>
+      <div className="footer-col d-flex flex-column">
+        <a href=""> acerca de <span>HeyDoc!</span></a>
+        <a href=""> Videoconsultas</a>
+        <a href=""> Innovación</a>
+        <a href=""> Ventajas y Beneficios</a>
+      </div>
+      <div className="footer-col d-flex flex-column">
+        <a href="">Opciones de Servicio</a>
+        <a href="">Contacto</a>
+      </div>
+      <div className="footer-col d-flex flex-column">
+        <a href="mailto:info@heydoc.com">info@heydoc.com</a>
+        <i className="bi bi-instagram"> HeyDoc</i>
+      </div>
     </footer>
   );
 };
+ 
