72483a01d30e4a186d0a4473c521f83cb77ddfcf
PHRCOOK <https://pabloelleproso@gmail.com>
2024-09-12 16:25:30 -0300

    modificacion de login

--- frontend/src/components/login/index.tsx
+++ frontend/src/components/login/index.tsx
@@ -21,7 +21,6 @@ const TextField: React.FC<{
   error?: string;
 }> = ({ id, name, type, value, onChange, onBlur, error }) => (
   <div className="input-container">
-    
     <input
       type={type}
       id={id}
@@ -30,10 +29,10 @@ const TextField: React.FC<{
       onChange={onChange}
       onBlur={onBlur}
       placeholder={name === "username" ? "Nombre de usuario" : "Contraseña"}
-      className={error? "input-register-error-pct":"input-register-pct"}
+      className={error ? "input-register-error-pct" : "input-register-pct"}
       required
     />
-    {error && <div className="error-message-pct">{error}</div>}{" "}
+    {error && <div className="error-message-pct">{error}</div>}
   </div>
 );
 
@@ -54,21 +53,20 @@ export const Login: React.FC = () => {
           password: values.password,
         });
 
-        // Desestructurar la respuesta para obtener userResponseDTO y token
         const { userResponseDTO, token } = response.data;
         const { roles } = userResponseDTO;
 
-        // Mostrar en consola la información deseada
-        console.log("User Response DTO:", userResponseDTO);
-        console.log("Token:", token);
-
-        // Suponiendo que deseas guardar el role y roleId en el contexto
         if (roles && roles.length > 0) {
-          const { roleId, roleName } = roles[0]; // Usar el primer rol como ejemplo
+          const { roleId, roleName } = roles[0];
           setToken(token);
           setRole(roleName);
           setRoleId(roleId);
-          console.log("Role ID:", roleId);
+
+          if (roleId === "9c765b7d-9eec-421b-85c6-6d53bcd002da") {
+            navigate("/dashboard/home");
+          } else {
+            navigate("/");
+          }
         }
 
         Swal.fire({
@@ -76,8 +74,6 @@ export const Login: React.FC = () => {
           title: "Éxito",
           text: "Has iniciado sesión correctamente.",
         });
-
-        navigate("/");
       } catch (err) {
         const errorMessage = axios.isAxiosError(err)
           ? "Error al iniciar sesión. Por favor, intenta nuevamente."
@@ -95,10 +91,17 @@ export const Login: React.FC = () => {
   return (
     <div className="register-user">
       <Link to="/" className="close-btn">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
-  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-</svg>
-        </Link>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          className="bi bi-x-lg"
+          viewBox="0 0 16 16"
+        >
+          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+        </svg>
+      </Link>
       <h2>Iniciar sesión</h2>
       <form onSubmit={formik.handleSubmit}>
         <TextField
@@ -120,9 +123,9 @@ export const Login: React.FC = () => {
           error={formik.touched.password ? formik.errors.password : undefined}
         />
         <div className="button-container">
-        <button className="btn-register-user" type="submit">
-          Continuar
-        </button>
+          <button className="btn-register-user" type="submit">
+            Continuar
+          </button>
         </div>
       </form>
     </div>
